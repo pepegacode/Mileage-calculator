3bbba5430d610f45b8d4b6f3aab15ebd3074b4a1
mahgree <mahgree@iu.edu>
2024-09-20 22:59:12 -0400

    Added part types and distinct ID numbers, also fixed a massive bug in assigning ID values

--- app.py
+++ app.py
@@ -10,11 +10,15 @@ class CarPartDatabase:
         self.tracks = {}
         self.load_data()
 
+    iterator=0
+
     def load_data(self):
         try:
             with open(self.filename, 'r', newline="") as file:
                 reader = csv.DictReader(file)
                 for row in reader:
+                    if row['type']=='util':
+                        CarPartDatabase.iterator=int(row['id'])
                     if row['type']=='kart':
                         kart_id = row['kart_id']
                         if kart_id not in self.karts:
@@ -46,6 +50,7 @@ class CarPartDatabase:
                             self.tracks[id] = {
                                 'name': row['name'],
                                 'type':'track',
+                                'details':row['details'],
                                 'mileage': row['mileage']
                             }
         except FileNotFoundError:
@@ -56,6 +61,24 @@ class CarPartDatabase:
             fieldnames = ['id', 'name','type', 'details', 'mileage', 'kart_id']
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
+            if CarPartDatabase.iterator == 0:
+                writer.writerow({  # Iterator used to generate ID numbers
+                    'id': '00000001',
+                    'name': 'iterator',
+                    'type':'util',
+                    'details': '',
+                    'mileage': '',
+                    'kart_id': ''
+                })
+            else:
+                writer.writerow({  # Iterator used to generate ID numbers
+                    'id': str('0'*(8-len(str(CarPartDatabase.iterator)))+str(CarPartDatabase.iterator)),
+                    'name': 'iterator',
+                    'type':'util',
+                    'details': '',
+                    'mileage': '',
+                    'kart_id': ''
+                })
             for part in self.parts:
                 writer.writerow(part)
             for kart_id, kart_data in self.karts.items():
@@ -72,19 +95,19 @@ class CarPartDatabase:
                     'id': id,
                     'name': track_data['name'],
                     'type':'track',
-                    'details': '',  # Track details are empty
+                    'details': track_data['details'],
                     'mileage': track_data['mileage'],
                     'kart_id': ''   #no kart id
                 })
 
     def add_kart(self, kart_name):
-        new_id = str(len(self.karts) + 1)
+        new_id = str(int(CarPartDatabase.iterator) + 1)
         self.karts[new_id] = {'id':new_id,'name': kart_name,'type':'kart', 'mileage': 0, 'kart_id': new_id}
         self.save_data()
-
-    def add_track(self, track_name, length):
-        new_id = str(len(self.tracks) + 1)
-        self.tracks[new_id] = {'name': track_name,'type':'track', 'mileage': length}
+    #add tracks needs to assign an ID that is always unique, right now it overwrites existing entries whenever you delete entries and reduce the total number of tracks below the id values, probably happens with karts as well and possibly parts
+    def add_track(self, track_name, length, track_details):
+        new_id = str(int(CarPartDatabase.iterator) + 1)
+        self.tracks[new_id] = {'name': track_name,'type':'track', 'details':track_details, 'mileage': length}
         self.save_data()
 
     def remove_kart(self, kart_id):
@@ -112,8 +135,34 @@ class CarPartDatabase:
                 i['kart_id']=0
         self.save_data()
 
-    def add_part(self, part_name, part_details):
-        new_id = str(len(self.parts) + 1)
+    def add_part(self, part_name, part_details, part_type):
+        partslist = [
+            "Sprocket",
+            "Sprocket Mount"
+            "Axle",
+            "Seat",
+            "Motor",
+            "Motor Mount",
+            "Filter",
+            "Chain",
+            "Clutch",
+            "Tie Rod",
+            "Steering Shaft",
+            "Spindle",
+            "Front Hub",
+            "Rear Hub"
+        ]
+        #Converts part type selection to part type number using parts list
+        type_id=0
+        ctr=0
+        for i in partslist:
+            ctr+=1
+            if str(part_type) == i:
+                type_id = ctr
+        if type_id==0:
+            messagebox.showerror("Error", "Error assigning part ID.")
+        #uses part type number and number of existing parts to assign id number, up to 3 zeroes are added in front of both numbers for a total of 8 digits
+        new_id =str("0"*(4-len(str(type_id)))) + str(type_id) + str("0"*(4-len(str(CarPartDatabase.iterator + 1)))) + str(CarPartDatabase.iterator + 1)
         self.parts.append({'id': new_id,'type':'part', 'name': part_name, 'details': part_details, 'mileage': 0, 'kart_id': 0})
         self.save_data()
 
@@ -149,7 +198,7 @@ class CarPartDatabase:
 class CarPartGUI:
     def __init__(self, root, database):
         self.root = root
-        self.root.title("Car Part Database")
+        self.root.title("Kart Part Database")
 
         self.database = database
         self.selected_kart = None  # Store the selected kart
@@ -168,10 +217,10 @@ class CarPartGUI:
         self.kart_frame.pack(side=tk.LEFT, padx=10, pady=10)
 
         self.parts_frame = tk.Frame(self.tab1)
-        self.parts_frame.pack(side=tk.RIGHT, padx=10, pady=10)
+        self.parts_frame.pack(side=tk.RIGHT, padx=10, pady=10, expand=1, fill = "both")
 
         self.track_frame = tk.Frame(self.tab2)
-        self.track_frame.pack(side=tk.RIGHT, padx=10, pady=10)
+        self.track_frame.pack(side=tk.RIGHT, padx=10, pady=10, expand=1, fill = "both")
 
         self.kart_frame2 = tk.Frame(self.tab2)
         self.kart_frame2.pack(side=tk.LEFT, padx=10, pady=10)
@@ -261,7 +310,7 @@ class CarPartGUI:
         self.kart_parts_label.pack()
 
         self.kart_parts_listbox = tk.Listbox(self.kart_frame,width=30)
-        self.kart_parts_listbox.pack()
+        self.kart_parts_listbox.pack(expand=1, fill = "both")
 
         self.kart_parts_listbox.bind("<<ListboxSelect>>", self.on_kart_part_selected)  # Bind selection event
         
@@ -295,8 +344,10 @@ class CarPartGUI:
     
     def on_track_selected(self, event):
         selected_track = self.track_listbox.curselection()
+        print(selected_track)
         if selected_track:
-            self.selected_track = list(self.database.karts.keys())[selected_track[0]]
+            self.selected_track = list(self.database.tracks.keys())[selected_track[0]]
+            print(self.selected_track)
             selected_track_name = self.database.tracks[self.selected_track]['name']
             self.selected_track_label.config(text=f"Selected Track: {selected_track_name}")
         else:
@@ -329,8 +380,8 @@ class CarPartGUI:
         self.parts_label = tk.Label(self.parts_frame, text="Parts Database:")
         self.parts_label.pack()
 
-        self.parts_listbox = tk.Listbox(self.parts_frame,width=60)
-        self.parts_listbox.pack()
+        self.parts_listbox = tk.Listbox(self.parts_frame,width=120)
+        self.parts_listbox.pack(expand=1, fill = "both")
 
         self.selected_part_label = tk.Label(self.parts_frame, text="Selected Part:")
         self.selected_part_label.pack()
@@ -356,6 +407,28 @@ class CarPartGUI:
         self.part_name_entry = tk.Entry(self.parts_frame)
         self.part_name_entry.pack()
 
+        self.options = [
+            "Sprocket",
+            "Sprocket Mount"
+            "Axle",
+            "Seat",
+            "Motor",
+            "Motor Mount",
+            "Filter",
+            "Chain",
+            "Clutch",
+            "Tie Rod",
+            "Steering Shaft",
+            "Spindle",
+            "Front Hub",
+            "Rear Hub"
+        ]
+
+        self.clicked = tk.StringVar()
+        self.clicked.set("Click to Assign Part Type")
+
+        self.part_type_entry = tk.OptionMenu(self.parts_frame,self.clicked,*self.options)
+        self.part_type_entry.pack()
 
 
         self.part_details_label = tk.Label(self.parts_frame, text="Part Details:")
@@ -381,7 +454,7 @@ class CarPartGUI:
         self.track_label.pack()
 
         self.track_listbox = tk.Listbox(self.track_frame,width=30)
-        self.track_listbox.pack()
+        self.track_listbox.pack(expand=1, fill = "both")
 
         self.selected_track_label = tk.Label(self.track_frame, text="Selected Track:")
         self.selected_track_label.pack()
@@ -398,6 +471,15 @@ class CarPartGUI:
 
         self.track_length_entry = tk.Entry(self.track_frame)
         self.track_length_entry.pack()
+        
+        self.track_details_label = tk.Label(self.track_frame, text="Track Details:")
+        self.track_details_label.pack()
+
+        self.track_details_entry = tk.Text(self.track_frame, height=4, width=30)
+        self.track_details_entry.pack()
+
+        self.track_details_label = tk.Label(self.track_frame, text="Track Details:")
+        self.track_details_label.pack()
 
         self.add_track_button = tk.Button(self.track_frame, text="Add Track", command=self.add_track)
         self.add_track_button.pack()
@@ -433,14 +515,15 @@ class CarPartGUI:
             try:
                 self.kart_listbox2.insert(tk.END, kart_name)
             except:
-                print("here")
+                
                 pass
 
     def refresh_tracks(self):
         self.track_listbox.delete(0, tk.END)
         for track_id,track_data in self.database.tracks.items():
-            track_name = f"{track_data['name']} - Length: {track_data['mileage']}"
+            track_name = f"Name: {track_data['name']} - Length: {track_data['mileage']} - Details: {track_data['details']}"
             self.track_listbox.insert(tk.END, track_name)
+
 
     def refresh_kart_parts(self, kart_id):
         self.kart_parts_listbox.delete(0, tk.END)
@@ -459,29 +542,35 @@ class CarPartGUI:
     def refresh_parts(self):
         self.parts_listbox.delete(0, tk.END)
         for part in self.database.parts:
-            part_name = f"{part['name']} - Mileage: {part['mileage']}"
+            part_name = f"ID: {part['id']} - Name: {part['name']} - Mileage: {part['mileage']}"
             self.parts_listbox.insert(tk.END, part_name)
 
     def add_kart(self):
         kart_name = self.kart_name_entry.get()
         if kart_name:
             self.database.add_kart(kart_name)
+            CarPartDatabase.iterator+=1
             self.kart_name_entry.delete(0, tk.END)
             self.refresh_karts()
 
     def add_track(self):
         track_name = self.track_name_entry.get()
+        track_details = self.track_details_entry.get("1.0", tk.END).strip()
         length = self.track_length_entry.get()
+        if track_details=='':
+            track_details="None"
         if track_name:
-            self.database.add_track(track_name,length)
+            self.database.add_track(track_name,length,track_details)
+            CarPartDatabase.iterator+=1
             self.track_name_entry.delete(0, tk.END)
             self.track_length_entry.delete(0, tk.END)
+            self.track_details_entry.delete("1.0", tk.END)
             self.refresh_tracks()
 
     def remove_kart(self):
         selected_kart = self.kart_listbox.curselection()
         if selected_kart:
-            kart_id = list(self.database.karts.keys())[selected_kart[0]]
+            kart_id = self.selected_kart
             self.database.remove_kart(kart_id)
             self.refresh_karts()
             self.kart_parts_listbox.delete(0, tk.END)
@@ -489,7 +578,10 @@ class CarPartGUI:
     def remove_track(self):
         selected_track = self.track_listbox.curselection()
         if selected_track:
-            track_id = list(self.database.karts.keys())[selected_track[0]]
+            track_id = self.selected_track
+            #track_id = list(self.database.karts.keys())[selected_track[0]]
+            print("HERE: "+self.selected_track)
+            print("HERE2: "+track_id)
             self.database.remove_track(track_id)
             self.refresh_tracks()
             self.kart_parts_listbox.delete(0, tk.END)
@@ -510,13 +602,19 @@ class CarPartGUI:
 
     def add_part(self):
         part_name = self.part_name_entry.get()
+        part_type = self.clicked.get()
         part_details = self.part_details_entry.get("1.0", tk.END).strip()
+        if part_type == "Click to Assign Part Type":
+            messagebox.showerror("Error", "Must assign Part Type.")
+            return
         if part_details=='':
             part_details="None"
         if part_name:
-            self.database.add_part(part_name, part_details)
+            self.database.add_part(part_name, part_details, part_type)
+            CarPartDatabase.iterator+=1
             self.part_name_entry.delete(0, tk.END)
             self.part_details_entry.delete("1.0", tk.END)
+            self.clicked.set("Click to Assign Part Type")
             self.refresh_parts()
 
 
@@ -536,6 +634,7 @@ class CarPartGUI:
         #if self.kart_mileage_entry.get() != '':
         #    manual = int(self.kart_mileage_entry.get())
         if self.track_laps_entry.get() != '':
+            print(self.selected_track)
             auto = int(self.track_laps_entry.get())*float(self.database.tracks[self.selected_track[0]]['mileage'])
             
         
